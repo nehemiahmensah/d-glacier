4448e20cd3a08cc8dd8c28c77d857c85ac99c7e4
nehemiahmensah <mensahnehemiah@gmail.com>
2023-11-13 05:05:54 +0000

    initial

---
+++ house.py
@@ -0,0 +1,15 @@
+# def get_expected_cost(beds,baths,basement):
+#     beds = beds * 30000
+#     baths = baths * 10000 
+#     basement = True or False
+#     total = 80000 + beds + baths 
+#     return total, f"{basement}"
+
+
+# get_expected_cost(0,2,True)
+def addm(a,b):
+    ans = a + b
+    return ans
+
+addm(2,5)
+
